9a61c8264d1bcaa68c9cd2f4eec7edecd4301ba6
Kenneth Shimabukuro <kenneth.r.shima@gmail.com>
2023-05-31 15:41:48 +0200

    allow media loading and displaying in django dev environment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'djoser',
-    
+
     'product'
 ]
 
@@ -128,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
