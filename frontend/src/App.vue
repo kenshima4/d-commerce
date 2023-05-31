1683858171f25fb736da1a3f1f4d2525577ff924
Kenneth Shimabukuro <kenneth.r.shima@gmail.com>
2023-05-31 15:05:04 +0200

    added basic header, footer and home page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,37 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+          <router-link to="/" class="navbar-item"><strong>d-commerce</strong></router-link>
+
+          <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+      </div>
+      <div class="navbar-menu" id="navbar-menu">
+        <div class="navbar-end">
+          <router-link to="/summer" class="navbar-item">Summer</router-link>
+          <router-link to="/winter" class="navbar-item">Winter</router-link>
+          <div class="buttons">
+            <router-link to="/log-in" class="button is-light">Login</router-link>
+            <router-link to="/cart" class="button is-success">
+              <span class="icon"><i clas="fas fa-shopping-cart"></i></span>
+              <span>Cart</span>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+  <section class="section">
+    <router-view/>
+  </section>
+  
+  <footer class="footer">
+    <p class="has-text-centered">Copyright (c) 2021</p>
+  </footer>
 </template>
 
 <style lang="scss">
