8209a2b7ab484dc1e5db7568b8018852d2603491
Kenneth Shimabukuro <kenneth.r.shima@gmail.com>
2023-06-01 12:52:17 +0200

    added get latest products for the frontend

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -9,18 +9,31 @@
                 d-lightful site for all your shopping needs
             </p>
         </div>
+    </section>
 
-        <div class="columns is-multiline">
+    <div class="columns is-multiline">
           <div class="column is-12">
               <h2 class="is-size-2 has-text-centered">Latest products</h2>
           </div>
 
-          <ProductBox 
-            v-for="product in latestProducts"
-            v-bind:key="product.id"
-            v-bind:product="product" />
+        <div class
+          v-for="product in latestProducts"
+          v-bind:key="product.id"
+          v-bind:product="product"
+        >
+          <div class="box">
+            <figure class="image mb-4">
+              <img v-bind:src="product.get_thumbnail">
+            </figure>
+
+            <h3 class="is-size-4">{{product.name}}</h3>
+
+            <p class="is-size-6 has-text-grey">${{ product.price  }}</p>
+
+            View Details
+          </div>
         </div>
-    </section>
+    </div>
 
   </div>
 </template>
@@ -31,8 +44,42 @@ import axios from 'axios'
 
 export default {
   name: 'HomeView',
-  components: {
-   
-  }
+  data() {
+    return {
+      latestProducts: []
+    }
+  },
+  components:{
+
+  }, 
+  mounted() {
+    this.getLatestProducts()
+
+    
+  },
+  methods: {
+    async getLatestProducts() {
+      this.$store.commit('setIsLoading', true)
+
+      await axios
+        .get('/api/v1/latest-products/')
+        .then(response => {
+          this.latestProducts = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+
+      
+    }
+  },
 }
 </script>
+
+<style scoped>
+  .image{
+    margin-top: -1.25rem;
+    margin-left: -1.25rem;
+    margin-right: -1.25rem;
+  }
+</style>
