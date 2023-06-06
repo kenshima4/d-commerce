8855ef3d3dcb0cac9827b789a2e5437c7332078d
Kenneth Shimabukuro <kenneth.r.shima@gmail.com>
2023-06-06 15:13:59 +0200

    show user confirmation message when adding item to cart

--- frontend/src/views/Product.vue
+++ frontend/src/views/Product.vue
@@ -32,7 +32,7 @@
 
 <script>
 import axios from 'axios'
-
+import { toast } from 'bulma-toast'
 export default{
     name : 'Product',
     data() {
@@ -69,7 +69,15 @@ export default{
             }
 
             this.$store.commit('addToCart', item)
-
+            
+            toast({
+                message: 'The product was added to the cart',
+                type: 'is-success',
+                dismissible: true,
+                pauseOnHover: true,
+                duration: 2000,
+                position: 'bottom-right',
+            })
             
         }
     }
