c93e2b29d65e97228fcef3572073a9c9594a07c6
Kenneth Shimabukuro <kenneth.r.shima@gmail.com>
2023-06-06 14:07:36 +0200

    created initial cart

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -18,7 +18,7 @@
             <router-link to="/log-in" class="button is-light">Login</router-link>
             <router-link to="/cart" class="button is-success">
               <span class="icon"><i clas="fas fa-shopping-cart"></i></span>
-              <span>Cart</span>
+              <span>Cart ({{ cartTotalLength }})</span>
             </router-link>
           </div>
         </div>
@@ -39,8 +39,25 @@ export default {
   data() {
     return {
       showMobileMenu: false,
+      cart:{
+        items: []
+      }
     }
   },
+  beforeCreate() {
+    this.$store.commit('initializeStore')
+  },
+  computed: {
+      cartTotalLength() {
+          let totalLength = 0
+
+          for (let i = 0; i < this.cart.items.length; i++) {
+              totalLength += this.cart.items[i].quantity
+          }
+
+          return totalLength
+      }
+  } 
 }
 </script>
 
