c65cf8c20f59cfc6736bcd52c93fc187138a156d
Kenneth Shimabukuro <kenneth.r.shima@gmail.com>
2023-06-14 14:45:28 +0200

    modified productbox to use cards and minor bug fix

--- frontend/src/components/ProductBox.vue
+++ frontend/src/components/ProductBox.vue
@@ -1,31 +1,64 @@
 <template>
     <div class="column is-3">
-        <div class="box">
-            <figure class="image mb-4">
-                <img v-bind:src="product.get_thumbnail">
-            </figure>
-
-            <h3 class="is-size-4">{{ product.name }}</h3>
-            <p class="is-size-6 has-text-grey">${{ product.price }}</p>
-
-            <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
+      <div class="card">
+        <div class="card-image">
+          <figure class="image">
+            <img :src="product.get_thumbnail" alt="Product Thumbnail" />
+          </figure>
         </div>
+  
+        <div class="card-content">
+          <h3 class="is-size-4 mb-2">{{ product.name }}</h3>
+          <p class="is-size-6 has-text-grey mb-4">${{ product.price }}</p>
+        </div>
+  
+        <footer class="card-footer">
+          <router-link :to="product.get_absolute_url" class="card-footer-item button is-dark">View details</router-link>
+        </footer>
+      </div>
     </div>
-</template>
-
-<script>
-export default {
+  </template>
+  
+  <script>
+  export default {
     name: 'ProductBox',
     props: {
-        product: Object
+      product: {
+        type: Object,
+        required: true
+      }
     }
-}
-</script>
-
-<style scoped>
-  .image {
-    margin-top: -1.25rem;
-    margin-left: -1.25rem;
-    margin-right: -1.25rem;
+  };
+  </script>
+  
+  <style scoped>
+  .column {
+    display: flex;
+    justify-content: center; /* Centers the cards horizontally */
   }
-</style>
+  
+  .card {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 1.5rem;
+    border: 1px solid #ccc; /* Optional: Add a border for visual separation */
+  }
+  
+  .card-content {
+    flex-grow: 1;
+  }
+  
+  .card-footer {
+    justify-content: center;
+  }
+  
+  .mb-2 {
+    margin-bottom: 0.5rem;
+  }
+  
+  .mb-4 {
+    margin-bottom: 1rem;
+  }
+  </style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -60,7 +60,7 @@ export default {
           console.log(error)
         })
 
-      
+      this.$store.commit('setIsLoading', false)
     }
   },
 }
